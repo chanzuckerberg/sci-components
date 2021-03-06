83cbef76d77bc108e4b8d8c02a06269219a7374e
Timmy Huang <thuang@chanzuckerberg.com>
2021-03-05 23:46:39 -0800

    add linters and prettier

--- src/core/Button/index.stories.tsx
+++ src/core/Button/index.stories.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
+import { storiesOf } from "@storybook/react";
+import React from "react";
 import Button from "./";
 
 export const text = "Click me!";
