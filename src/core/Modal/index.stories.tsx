6739101d325d7cb09163df40109131f85b824e85
ovalenzuela19 <ovalenzuela@chanzuckerberg.com>
2021-03-10 15:21:43 -0800

    generate stories for other widths

--- src/core/Modal/index.stories.tsx
+++ src/core/Modal/index.stories.tsx
@@ -1,11 +1,11 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import Button from "../Button";
-import Modal from "./index";
+import Modal, { WIDTHS } from "./index";
 
 export const text = "This is a modal";
 
-const DefaultModal = () => {
+const DefaultModal = ({ width = WIDTHS.DEFAULT } = {}) => {
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => setOpen(true);
@@ -16,7 +16,7 @@ const DefaultModal = () => {
       <Button color="primary" onClick={handleOpen}>
         Open modal
       </Button>
-      <Modal open={open} onClose={handleClose}>
+      <Modal width={width} open={open} onClose={handleClose}>
         <h2 id="simple-modal-title">Text in a modal</h2>
         <p id="simple-modal-description">
           Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
@@ -26,4 +26,15 @@ const DefaultModal = () => {
   );
 };
 
-storiesOf("Modal", module).add("default", () => DefaultModal());
+storiesOf("Modal", module).add("narrow", () =>
+  DefaultModal({ width: WIDTHS.NARROW })
+);
+storiesOf("Modal", module).add("default", () =>
+  DefaultModal({ width: WIDTHS.DEFAULT })
+);
+storiesOf("Modal", module).add("wide", () =>
+  DefaultModal({ width: WIDTHS.WIDE })
+);
+storiesOf("Modal", module).add("widest", () =>
+  DefaultModal({ width: WIDTHS.WIDEST })
+);
