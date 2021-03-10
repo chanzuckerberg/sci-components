6fa91bce4479e7c6f3d2abfe34425e9b96fcfe70
Jonathan Sheu <jsheu@chanzuckerberg.com>
2021-03-10 11:07:48 -0800

    Checkbox

--- src/core/Checkbox/index.stories.tsx
+++ src/core/Checkbox/index.stories.tsx
@@ -1,28 +1,40 @@
+import css from "@emotion/css";
 import styled from "@emotion/styled";
+import { makeStyles } from "@material-ui/core/styles";
 import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import Checkbox from ".";
 
+const base = css`
+  color: hotpink;
+`;
+console.log(base);
+
 export const actions = {
   onClick: action("onClick"),
 };
 
+const useStyles = makeStyles({
+  root: {
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+});
+
 const StyledCheckbox = styled(Checkbox)`
-  // padding: 32px;
-  // background-color: blue;
-  // font-size: 24px;
-  // border-radius: 10px;
-  // color: black;
-  // font-weight: bold;
   &:hover {
     background-color: transparent;
 
     &.Mui-checked {
-      background-color: transparent;
+      // last resort!
+      // background-color: transparent;
     }
   }
 `;
+
+// const classes = useStyles();
 
 storiesOf("Checkbox", module).add("primary", () => (
   <StyledCheckbox color="primary" {...actions} />
