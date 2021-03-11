25f7676b996c24472e59547a7d8a850810496c40
Jonathan Sheu <jsheu@chanzuckerberg.com>
2021-03-10 16:16:34 -0800

    Tweak

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "@material-ui/core";
 export * as Button from "./core/Button";
 export * as Checkbox from "./core/Checkbox";
-export * from "./core/styles";
+export { defaultTheme, defaultThemeOptions } from "./core/styles";
