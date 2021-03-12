e2ce85589021a3e8f264372618521f750df1139d
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-03-12 10:13:01 -0800

    create tab

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "@material-ui/core";
 export * as Button from "./core/Button";
 export * as Checkbox from "./core/Checkbox";
 export * as defaultTheme from "./core/styles";
+export * as Tabs from "./core/Tabs";
