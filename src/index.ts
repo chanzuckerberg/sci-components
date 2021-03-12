9734ca3a3219406492f3ef996bbc234960ebd471
Timmy Huang <thuang@chanzuckerberg.com>
2021-03-12 13:35:38 -0800

    thuang-fix-multiselect

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from "@material-ui/core";
 export * as Button from "./core/Button";
 export * as Checkbox from "./core/Checkbox";
 export * as defaultTheme from "./core/styles";
-export * as Tabs from "./core/Tabs";
 export * from "./core/styles/index";
+export * as Tabs from "./core/Tabs";
