9a9b97ad486c51a1c248e1da5d173da0f1f0e0c0
Timmy Huang <tihuan@users.noreply.github.com>
2021-05-03 15:45:02 -0700

    Thuang add list and link (#20)
    * export getFontWeights
    
    * 0.0.26
    
    * export ListItemLabel
    
    * 0.0.27
    
    * add ordered list
    
    * 0.0.28

--- src/core/List/index.stories.tsx
+++ src/core/List/index.stories.tsx
@@ -6,38 +6,95 @@ import List from "./index";
 
 const Demo = (props: Args): JSX.Element => {
   return (
-    <List
-      subheader={<ListSubheader disableSticky>This is a list</ListSubheader>}
-      {...props}
-    >
-      <ListItem fontSize="l" marginBottom="s">
-        fontSize=l marginBottom=s
-      </ListItem>
-      <ListItem fontSize="m" marginBottom="s">
-        fontSize=m marginBottom=s
-      </ListItem>
-      <ListItem fontSize="s" marginBottom="xs">
-        fontSize=s marginBottom=xs
-      </ListItem>
-      <ListItem fontSize="s" marginBottom="xs">
-        fontSize=s marginBottom=xs
-      </ListItem>
-      <ListItem fontSize="xs" marginBottom="xs">
-        fontSize=xs marginBottom=xs
-      </ListItem>
-      <ListItem fontSize="xxs" marginBottom="xs">
-        fontSize=xxs marginBottom=xs
-      </ListItem>
-      <ListItem fontSize="xxxs" marginBottom="xxs">
-        fontSize=xxxs marginBottom=xxs
-      </ListItem>
-      <ListItem fontSize="m" marginBottom="s">
-        <div style={{ width: "200px" }}>
-          Really long list item here to make it wrap, so we can see if the
-          bullet is top aligned
-        </div>
-      </ListItem>
-    </List>
+    <div>
+      <List
+        subheader={<ListSubheader disableSticky>This is a list</ListSubheader>}
+        {...props}
+      >
+        <ListItem fontSize="l" marginBottom="s">
+          fontSize=l marginBottom=s
+        </ListItem>
+        <ListItem fontSize="m" marginBottom="s">
+          fontSize=m marginBottom=s
+        </ListItem>
+        <ListItem fontSize="s" marginBottom="xs">
+          fontSize=s marginBottom=xs
+        </ListItem>
+        <ListItem fontSize="xs" marginBottom="xs">
+          fontSize=xs marginBottom=xs
+        </ListItem>
+        <ListItem fontSize="xxs" marginBottom="xs">
+          fontSize=xxs marginBottom=xs
+        </ListItem>
+        <ListItem fontSize="xxxs" marginBottom="xxs">
+          fontSize=xxxs marginBottom=xxs
+        </ListItem>
+        <ListItem fontSize="m" marginBottom="s">
+          <div style={{ width: "200px" }}>
+            Really long list item here to make it wrap, so we can see if the
+            bullet is top aligned
+          </div>
+        </ListItem>
+      </List>
+      <List
+        ordered
+        subheader={
+          <ListSubheader disableSticky>
+            This is a nested ordered list
+          </ListSubheader>
+        }
+        {...props}
+      >
+        <ListItem ordered>
+          <span>
+            Nested List 1
+            <List ordered>
+              <ListItem ordered fontSize="l" marginBottom="s">
+                fontSize=l marginBottom=s
+              </ListItem>
+              <ListItem ordered fontSize="m" marginBottom="s">
+                fontSize=m marginBottom=s
+              </ListItem>
+              <ListItem ordered fontSize="s" marginBottom="xs">
+                fontSize=s marginBottom=xs
+              </ListItem>
+            </List>
+          </span>
+        </ListItem>
+        <ListItem ordered>
+          <span>
+            Nested List 2
+            <List ordered>
+              <ListItem ordered fontSize="l" marginBottom="s">
+                fontSize=l marginBottom=s
+              </ListItem>
+              <ListItem ordered fontSize="m" marginBottom="s">
+                fontSize=m marginBottom=s
+              </ListItem>
+              <ListItem ordered fontSize="s" marginBottom="xs">
+                fontSize=s marginBottom=xs
+              </ListItem>
+            </List>
+          </span>
+        </ListItem>
+        <ListItem ordered>
+          <span>
+            Nested List 3
+            <List ordered>
+              <ListItem ordered fontSize="l" marginBottom="s">
+                fontSize=l marginBottom=s
+              </ListItem>
+              <ListItem ordered fontSize="m" marginBottom="s">
+                fontSize=m marginBottom=s
+              </ListItem>
+              <ListItem ordered fontSize="s" marginBottom="xs">
+                fontSize=s marginBottom=xs
+              </ListItem>
+            </List>
+          </span>
+        </ListItem>
+      </List>
+    </div>
   );
 };
 
--- src/core/ListItem/style.ts
+++ src/core/ListItem/style.ts
@@ -2,7 +2,7 @@ import { SerializedStyles } from "@emotion/react";
 import styled from "@emotion/styled";
 import { ListItem } from "@material-ui/core";
 import { TypographyOptions } from "@material-ui/core/styles/createTypography";
-import { fontBody, getSpacings, Props } from "../styles";
+import { fontBody, getFontWeights, getSpacings, Props } from "../styles";
 
 const fontBodyL = fontBody("l");
 const fontBodyM = fontBody("m");
@@ -14,10 +14,11 @@ const fontBodyXxxs = fontBody("xxxs");
 export interface ExtraProps extends Props {
   marginBottom?: "s" | "xs" | "xxs";
   fontSize?: "xxxs" | "xxs" | "xs" | "s" | "m" | "l";
+  ordered?: boolean;
 }
 
 // (thuang): Please keep this in sync with the props used in `ExtraProps`
-const doNotForwardProps = ["marginBottom", "fontSize"];
+const doNotForwardProps = ["marginBottom", "fontSize", "ordered"];
 
 export const StyledListItem = styled(ListItem, {
   shouldForwardProp: (prop) => !doNotForwardProps.includes(prop as string),
@@ -30,6 +31,7 @@ export const StyledListItem = styled(ListItem, {
 
     ${(props) => {
       const spacings = getSpacings(props);
+      const { ordered } = props;
 
       const {
         theme: { typography },
@@ -37,27 +39,33 @@ export const StyledListItem = styled(ListItem, {
 
       return `
         font-family: ${(typography as TypographyOptions).fontFamily};
-        margin-left: ${spacings?.m as number}px;
+        margin-left: ${markerMargin(Boolean(ordered), spacings?.m || 1)}px;
+        ${ordered ? "counter-increment: section;" : ""}
       `;
     }}
 
     &:before {
-      content: "•";
       display: inline-block;
       font-weight: 600;
       position: absolute;
+      top: 0;
 
       ${(props) => {
         const spacings = getSpacings(props);
+        const { ordered } = props;
 
         return `
-          top: 0;
-          left: -${spacings?.m}px;
-      `;
+          content: ${ordered ? `counters(section, ".")` : `"•"`};
+          left: -${markerMargin(Boolean(ordered), spacings?.m || 1)}px;
+        `;
       }}
     }
   }
 `;
+
+function markerMargin(ordered: boolean, spacing = 1) {
+  return ordered ? 3 * spacing : spacing;
+}
 
 function propsToFontBody(props: ExtraProps) {
   const propsToFontBodyMap: Record<
@@ -95,3 +103,15 @@ function propsToMarginBottom(props: ExtraProps) {
     margin-bottom: ${propsToMarginBottomMap[marginBottom || "xs"]}px;
   `;
 }
+
+export const ListItemLabel = styled.span`
+  margin-right: 5px;
+
+  ${(props) => {
+    const fontWeights = getFontWeights(props);
+
+    return `
+      font-weight: ${fontWeights?.semibold};
+    `;
+  }}
+`;
