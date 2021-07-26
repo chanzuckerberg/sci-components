cbc66b64527c12c4b2613da25cc6f7063bfbbf7a
Erin Hoops <erin.hoops@chanzuckerberg.com>
2021-07-26 11:52:56 -0700

    Checkbox update wip

--- src/core/Checkbox/index.stories.tsx
+++ src/core/Checkbox/index.stories.tsx
@@ -1,4 +1,3 @@
-import styled from "@emotion/styled";
 import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
 import React from "react";
@@ -8,16 +7,92 @@ const actions = {
   onClick: action("onClick"),
 };
 
-const StyledCheckbox = styled(Checkbox)`
-  &:hover {
-    background-color: transparent;
-
-    &.Mui-checked {
-      background-color: transparent;
-    }
-  }
-`;
+storiesOf("Checkbox", module).add("default", () => (
+  <>
+    <p>
+      Accessibility: All checkboxes should have a label. This can be done with a
+      visible form label or with the aria-label attribute.
+      https://material-ui.com/components/checkboxes/#accessibility
+    </p>
+    <Checkbox
+      color="default"
+      defaultChecked
+      inputProps={{ "aria-label": "checkbox-default-checked" }}
+      onClick={actions.onClick}
+    />
+    <Checkbox
+      color="default"
+      inputProps={{ "aria-label": "checkbox" }}
+      onClick={actions.onClick}
+    />
+    <Checkbox
+      color="default"
+      disabled
+      inputProps={{ "aria-label": "checkbox-disabled" }}
+      onClick={actions.onClick}
+    />
+  </>
+));
 
 storiesOf("Checkbox", module).add("primary", () => (
-  <StyledCheckbox color="primary" onClick={actions.onClick} />
+  <>
+    <Checkbox
+      color="primary"
+      defaultChecked
+      inputProps={{ "aria-label": "primary-checkbox-default-checked" }}
+      onClick={actions.onClick}
+    />
+    <Checkbox
+      color="primary"
+      inputProps={{ "aria-label": "primary-checkbox" }}
+      onClick={actions.onClick}
+    />
+    <Checkbox
+      color="primary"
+      disabled
+      inputProps={{ "aria-label": "primary-checkbox-disabled" }}
+      onClick={actions.onClick}
+    />
+  </>
+));
+
+storiesOf("Checkbox", module).add("secondary", () => (
+  <>
+    <Checkbox
+      color="secondary"
+      defaultChecked
+      inputProps={{ "aria-label": "secondary-checkbox-default-checked" }}
+      onClick={actions.onClick}
+    />
+    <Checkbox
+      color="secondary"
+      inputProps={{ "aria-label": "secondary-checkbox" }}
+      onClick={actions.onClick}
+    />
+    <Checkbox
+      color="secondary"
+      disabled
+      inputProps={{ "aria-label": "secondary-checkbox-disabled" }}
+      onClick={actions.onClick}
+    />
+  </>
+));
+
+storiesOf("Checkbox", module).add("indeterminate", () => (
+  <>
+    <Checkbox
+      color="primary"
+      defaultChecked
+      indeterminate
+      inputProps={{ "aria-label": "primary-checkbox-indeterminate" }}
+      onClick={actions.onClick}
+    />
+    <Checkbox
+      color="secondary"
+      defaultChecked
+      indeterminate
+      inputProps={{ "aria-label": "secondary-checkbox-indeterminate" }}
+      onClick={actions.onClick}
+    />
+  </>
 ));
--- src/core/Checkbox/index.tsx
+++ src/core/Checkbox/index.tsx
@@ -1,10 +1,30 @@
+import styled from "@emotion/styled";
 import { Checkbox as RawCheckbox, CheckboxProps } from "@material-ui/core";
 import React from "react";
+import { getColors } from "../styles";
 
 export { CheckboxProps };
 
+const StyledCheckbox = styled(RawCheckbox)`
+  ${(props) => {
+    const colors = getColors(props);
+    return `
+      color: ${colors?.gray[300]};
+      &:hover {
+        color: ${colors?.gray[400]};
+        background-color: transparent;
+      }
+      &.Mui-checked {
+        background-color: transparent;
+      }
+    `;
+  }}
+  &:hover {
+  }
+`;
+
 const Checkbox = (props: CheckboxProps): JSX.Element => {
-  return <RawCheckbox {...props} />;
+  return <StyledCheckbox {...props} />;
 };
 
 export default Checkbox;
