0251c19b1c82308c7520ba1eef902b7e0be6cd76
Julie Han <53838890+j-x-han@users.noreply.github.com>
2021-08-04 17:42:49 -0700

    [CH-148810] Implement large square Tag (#39)

--- src/core/Chip/index.stories.tsx
+++ src/core/Chip/index.stories.tsx
@@ -1,9 +1,27 @@
 import { Args, Story } from "@storybook/react";
 import React from "react";
+import Button from "../Button";
 import Chip from "./index";
 
 const Demo = (props: Args): JSX.Element => {
-  return <Chip size="small" label="default small" {...props} />;
+  const { size, isRounded } = props;
+  const [visible, setVisible] = React.useState(false);
+
+  const handleShowChip = () => setVisible(true);
+  const handleDismissChip = () => setVisible(false);
+
+  if (size === "medium" && !isRounded) {
+    return (
+      <>
+        <Button variant="contained" onClick={handleShowChip}>
+          Click me!
+        </Button>
+        {visible && <Chip onDelete={handleDismissChip} {...props} />}
+      </>
+    );
+  } else {
+    return <Chip {...props} />;
+  }
 };
 
 export default {
@@ -13,44 +31,64 @@ export default {
 
 const Template: Story = (args) => <Demo {...args} />;
 
+export const MediumSquareChip = Template.bind({});
+
+MediumSquareChip.args = {
+  isRounded: false,
+  label: "Chip",
+  size: "medium",
+};
+
 export const Success = Template.bind({});
 
 Success.args = {
+  isRounded: true,
   label: "success",
+  size: "small",
   status: "success",
 };
 
 export const Error = Template.bind({});
 
 Error.args = {
+  isRounded: true,
   label: "error",
+  size: "small",
   status: "error",
 };
 
 export const Warning = Template.bind({});
 
 Warning.args = {
+  isRounded: true,
   label: "warning",
+  size: "small",
   status: "warning",
 };
 
 export const Info = Template.bind({});
 
 Info.args = {
+  isRounded: true,
   label: "info",
+  size: "small",
   status: "info",
 };
 
 export const Pending = Template.bind({});
 
 Pending.args = {
+  isRounded: true,
   label: "pending",
+  size: "small",
   status: "pending",
 };
 
 export const Beta = Template.bind({});
 
 Beta.args = {
+  isRounded: true,
   label: "beta",
+  size: "small",
   status: "beta",
 };
