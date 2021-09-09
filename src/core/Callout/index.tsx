2ab167444e396f50ba34e7a409e37c615e9392fc
Meagan Moore <816moore@gmail.com>
2021-09-09 13:41:56 -0400

    move icon to base component

--- src/core/Callout/index.tsx
+++ src/core/Callout/index.tsx
@@ -1,11 +1,12 @@
 import { AlertProps } from "@material-ui/lab";
 import React from "react";
+import { ReactComponent as IconAlert } from "../../common/svgs/IconAlert.svg";
 import { StyledCallout } from "./style";
 
 export { AlertProps };
 
 const Callout = (props: AlertProps): JSX.Element => {
-  return <StyledCallout {...props} />;
+  return <StyledCallout {...props} icon={<IconAlert fillContrast="white" />} />;
 };
 
 export default Callout;
