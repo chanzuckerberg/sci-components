c3122354b99c57b94180b23b86db6ba28fc7f20a
Meagan Moore <816moore@gmail.com>
2021-09-20 14:18:43 -0400

    icon update and Aspen implementation test

--- src/core/Notification/index.tsx
+++ src/core/Notification/index.tsx
@@ -1,20 +1,29 @@
 import { AlertProps } from "@material-ui/lab";
 import React from "react";
 import { ReactComponent as IconAlert } from "../../common/svgs/IconAlert.svg";
+import { ReactComponent as IconSuccess } from "../../common/svgs/IconSuccess.svg";
 import { StyledNotification, StyledNotificationTitle } from "./style";
 
 export { AlertProps };
 
 const Notification = ({
   children,
+  severity,
   title,
   ...rest
 }: AlertProps): JSX.Element => {
   return (
     <StyledNotification
       {...rest}
-      icon={<IconAlert fillContrast="white" />}
+      icon={
+        severity === "success" ? (
+          <IconSuccess fillContrast="white" />
+        ) : (
+          <IconAlert fillContrast="white" />
+        )
+      }
       className="elevated"
+      severity={severity}
     >
       <StyledNotificationTitle>{title}</StyledNotificationTitle>
       {children}
