0c28f65eb298407998e3c971bba7267f297775c0
Meagan Moore <816moore@gmail.com>
2021-10-14 10:20:43 -0400

    test(global): speed up tests
    set isolatedModules to true to decrese test time

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
   transform: {
     "^.+\\.svg$": "svg-jest",
   },
