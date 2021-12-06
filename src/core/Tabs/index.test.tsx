7d2e1a2a64e095766853a6dc3afdd53fac44fb6a
Timmy Huang <thuang@chanzuckerberg.com>
2021-12-06 15:07:42 -0800

    fix(tabs): update Tabs to match style specs

---
+++ src/core/Tabs/index.test.tsx
@@ -0,0 +1,31 @@
+import { generateSnapshots } from "@chanzuckerberg/story-utils";
+import { composeStory } from "@storybook/testing-react";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import * as snapshotTestStoryFile from "./index.stories";
+import Meta, { Test as TestStory } from "./index.stories";
+
+// Returns a component that already contain all decorators from story level, meta level and global level.
+const Test = composeStory(TestStory, Meta);
+
+describe("<Tabs />", () => {
+  generateSnapshots(snapshotTestStoryFile);
+
+  it("renders Tabs component", () => {
+    render(<Test />);
+
+    const element = screen.getByTestId("tabs");
+
+    expect(element).not.toBeNull();
+  });
+
+  it("renders tab texts", () => {
+    render(<Test />);
+
+    const Label1 = screen.getByText(/Tab One/i);
+    const Label2 = screen.getByText(/Tab Two/i);
+
+    expect(Label1).toBeTruthy();
+    expect(Label2).toBeTruthy();
+  });
+});
