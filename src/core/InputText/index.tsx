57117d69e86f770ea90e0eee7fc35b33c59b3fc8
Meagan Moore <816moore@gmail.com>
2022-03-01 16:08:21 -0500

    refactor(inputtext): update forward ref as per PR 130
    see https://github.com/chanzuckerberg/sci-components/pull/130 for context

--- src/core/InputText/index.tsx
+++ src/core/InputText/index.tsx
@@ -1,5 +1,5 @@
 import { TextFieldProps as RawTextFieldProps } from "@material-ui/core";
-import React, { ForwardedRef, forwardRef, useState } from "react";
+import React, { forwardRef, useState } from "react";
 import { ExtraProps, StyledInputBase, StyledLabel } from "./style";
 
 interface AccessibleInputTextProps {
@@ -12,63 +12,62 @@ export type InputTextProps = RawTextFieldProps &
   ExtraProps &
   AccessibleInputTextProps;
 
-const InputText = (
-  props: InputTextProps,
-  ref: ForwardedRef<HTMLInputElement>
-): JSX.Element => {
-  const {
-    id,
-    intent = "default",
-    label,
-    placeholder,
-    sdsType,
-    hideLabel,
-    ...rest
-  } = props;
+const InputText = forwardRef<HTMLInputElement, InputTextProps>(
+  function InputText(props, ref): JSX.Element {
+    const {
+      id,
+      intent = "default",
+      label,
+      placeholder,
+      sdsType,
+      hideLabel,
+      ...rest
+    } = props;
 
-  const [hasValue, setHasValue] = useState<boolean>(false);
+    const [hasValue, setHasValue] = useState<boolean>(false);
 
-  const handleChange = (event: { target: { value: string } }) => {
-    if (event.target.value) {
-      setHasValue(true);
-    } else {
-      setHasValue(false);
+    const handleChange = (event: { target: { value: string } }) => {
+      if (event.target.value) {
+        setHasValue(true);
+      } else {
+        setHasValue(false);
+      }
+    };
+
+    const inputProps = {
+      "aria-label": `${label}`,
+    };
+
+    if (!id || !label) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `Error: czifui component InputText requires id and label props for accessibility.`
+      );
+      return <></>;
     }
-  };
 
-  const inputProps = {
-    "aria-label": `${label}`,
-  };
-
-  if (!id || !label) {
-    // eslint-disable-next-line no-console
-    console.error(
-      `Error: czifui component InputText requires id and label props for accessibility.`
+    return (
+      <>
+        {!hideLabel && <StyledLabel htmlFor={id}>{label}</StyledLabel>}
+        <StyledInputBase
+          ref={ref}
+          inputProps={inputProps}
+          type="text"
+          multiline={sdsType === "textArea" ? true : false}
+          minRows={sdsType === "textArea" ? 4 : 2}
+          id={id}
+          intent={intent}
+          variant="outlined"
+          size="small"
+          placeholder={placeholder}
+          sdsStage={hasValue ? "userInput" : "default"}
+          sdsType={sdsType}
+          onChange={handleChange}
+          {...rest}
+        />
+      </>
     );
-    return <></>;
   }
+);
 
-  return (
-    <>
-      {!hideLabel && <StyledLabel htmlFor={id}>{label}</StyledLabel>}
-      <StyledInputBase
-        ref={ref}
-        inputProps={inputProps}
-        type="text"
-        multiline={sdsType === "textArea" ? true : false}
-        minRows={sdsType === "textArea" ? 4 : 2}
-        id={id}
-        intent={intent}
-        variant="outlined"
-        size="small"
-        placeholder={placeholder}
-        sdsStage={hasValue ? "userInput" : "default"}
-        sdsType={sdsType}
-        onChange={handleChange}
-        {...rest}
-      />
-    </>
-  );
-};
-
-export default forwardRef(InputText);
+export default InputText;
