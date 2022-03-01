445be62db81b559494c6507b5b5b34503b710409
Meagan Moore <35696944+mdjmoore@users.noreply.github.com>
2022-03-01 16:19:52 -0500

    feat(inputtext): add new inputText component (#126)
    * feat(inputtext): add new component
    
    * test(inputtext): add defaultm livepreview and test stories
    
    * test(inputtext): add tests
    
    * style(inputtext): lint
    
    * style(inputtext): lint errors
    
    * style(inputtext): lint
    
    * test(inputtext): update snaps
    
    * refactor(inputtext): lint errors
    
    * style(inputtext): chromatic feedback
    
    * test(inputtext): update snaps
    
    * style(inputtext): pr feedback on resize
    
    * style(tooltip): refactor tooltip placement story and tooltip arrow styling
    
    * style(tooltip): more margin in stories
    
    * fix(inputtext): chromatic feedback - resize bug affected other inputs
    
    * fix(inputtext): fix export name
    
    * style(inputtext): chromatic feedback, margin on label
    
    * refactor(inputtext): update forward ref as per PR 130
    
    see https://github.com/chanzuckerberg/sci-components/pull/130 for context
    
    Co-authored-by: Timmy Huang <tihuan@users.noreply.github.com>

---
+++ src/core/InputText/index.tsx
@@ -0,0 +1,73 @@
+import { TextFieldProps as RawTextFieldProps } from "@material-ui/core";
+import React, { forwardRef, useState } from "react";
+import { ExtraProps, StyledInputBase, StyledLabel } from "./style";
+
+interface AccessibleInputTextProps {
+  label: string;
+  placeholder?: string;
+  id: string;
+}
+
+export type InputTextProps = RawTextFieldProps &
+  ExtraProps &
+  AccessibleInputTextProps;
+
+const InputText = forwardRef<HTMLInputElement, InputTextProps>(
+  function InputText(props, ref): JSX.Element {
+    const {
+      id,
+      intent = "default",
+      label,
+      placeholder,
+      sdsType,
+      hideLabel,
+      ...rest
+    } = props;
+
+    const [hasValue, setHasValue] = useState<boolean>(false);
+
+    const handleChange = (event: { target: { value: string } }) => {
+      if (event.target.value) {
+        setHasValue(true);
+      } else {
+        setHasValue(false);
+      }
+    };
+
+    const inputProps = {
+      "aria-label": `${label}`,
+    };
+
+    if (!id || !label) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `Error: czifui component InputText requires id and label props for accessibility.`
+      );
+      return <></>;
+    }
+
+    return (
+      <>
+        {!hideLabel && <StyledLabel htmlFor={id}>{label}</StyledLabel>}
+        <StyledInputBase
+          ref={ref}
+          inputProps={inputProps}
+          type="text"
+          multiline={sdsType === "textArea" ? true : false}
+          minRows={sdsType === "textArea" ? 4 : 2}
+          id={id}
+          intent={intent}
+          variant="outlined"
+          size="small"
+          placeholder={placeholder}
+          sdsStage={hasValue ? "userInput" : "default"}
+          sdsType={sdsType}
+          onChange={handleChange}
+          {...rest}
+        />
+      </>
+    );
+  }
+);
+
+export default InputText;
