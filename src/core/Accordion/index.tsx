cc6c44eaff75baa38c097ddf079dae929afbe877
Meagan Moore <35696944+mdjmoore@users.noreply.github.com>
2022-05-05 12:40:46 -0400

    feat(accordion): add component and sub components (#146)
    * feat(accordion): add component and sub component
    
    * feat(accordion): add header, details components, styles, sds props
    
    * feat(accordion): add togglePosition
    
    * test(accordion): add tests, make component controllable, finish prop handling
    
    * chore(accordion): add livePreview
    
    * test(accordion): update snaps
    
    * test(accordion): update snaps
    
    * feat(accordion): pr feedback: adjust spacing
    
    * test(accordion): update snaps
    
    * fix(accordion): pR feedback, spacing of content module and toggle
    
    * test(accordion): snaps
    
    Co-authored-by: maya <mlila@chanzuckerberg.com>

---
+++ src/core/Accordion/index.tsx
@@ -0,0 +1,31 @@
+import { AccordionProps as RawAccordionProps } from "@material-ui/core";
+import React from "react";
+import { ExtraProps, StyledAccordion } from "./style";
+
+export type AccordionProps = RawAccordionProps & ExtraProps;
+
+const Accordion = (props: AccordionProps) => {
+  const { children, useDivider, togglePosition = "right", id } = props;
+  const [expanded, setExpanded] = React.useState<string | false>(false);
+
+  const handleChange =
+    (panel: string) =>
+    (_event: React.ChangeEvent<unknown>, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+  return (
+    <StyledAccordion
+      id={id}
+      square
+      useDivider={useDivider}
+      togglePosition={togglePosition}
+      expanded={expanded === id}
+      onChange={handleChange(id)}
+      data-testid="accordion"
+    >
+      {children}
+    </StyledAccordion>
+  );
+};
+
+export default Accordion;
