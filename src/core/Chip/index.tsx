e57ebc53eff98ff955c914cb156ff419d9d51122
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2022-05-18 00:10:00 -0400

    refactor(tag): flag old Chip component as To Be Depreciated

--- src/core/Chip/index.tsx
+++ src/core/Chip/index.tsx
@@ -7,11 +7,21 @@ type ChipProps = ChipExtraProps & RawChipProps;
 
 export type { ChipProps };
 
+let hasWarned = false;
+
 /**
  * @see https://v4.mui.com/components/chips/
  */
 const Chip = (props: ChipProps): JSX.Element => {
+  if (!hasWarned) {
+    // eslint-disable-next-line no-console
+    console.warn(
+      "Warning: <Chip /> will be deprecated and replaced with <Tag />"
+    );
+    hasWarned = true;
+  }
   const { onDelete } = props;
+
   if (onDelete) {
     return <StyledChip deleteIcon={<Clear />} {...props} />;
   }
