a5f7e98176ba5bf6cc9782879845de5356826df2
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2022-08-18 13:19:12 -0400

    style(table): remove comments from style.ts

--- src/core/Table/style.ts
+++ src/core/Table/style.ts
@@ -1,7 +1,6 @@
 import { styled } from "@mui/material/styles";
 
 export const StyledTable = styled("table")`
-  // masoud: This will be removed after using Row components
   tr {
     display: flex;
   }
