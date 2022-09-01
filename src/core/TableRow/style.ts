5ecc47bab84e31a7297b8afc3165cff085c41633
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2022-08-31 21:55:43 -0400

    feat(TableRow): Table Row component implementation (#242)
    * feat(row): table Row component implementation
    
    * refactor(row): use BasicCell in stories + Code clean up
    
    * style(row): chnage min-height to max-height on the Row component
    
    * chore(row): remove extra whitespaces
    
    * feat(row): add hover prop to row component
    
    * refactor(row): update types
    
    * refactor(row): update BasicCell to CellBasic
    
    * refactor(row): chnage component name to TableRow

---
+++ src/core/TableRow/style.ts
@@ -0,0 +1,74 @@
+import { styled } from "@mui/material/styles";
+import React from "react";
+import { CommonThemeProps, fontHeaderS, getColors } from "../styles";
+
+export interface RowExtraProps
+  extends CommonThemeProps,
+    Omit<React.HTMLProps<HTMLTableRowElement>, "as"> {
+  rowHeight?: number;
+  useDivider?: boolean;
+  selected?: boolean;
+  disabled?: boolean;
+  hover?: boolean;
+}
+
+const doNotForwardProps = [
+  "rowHeight",
+  "useDivider",
+  "shouldShowTooltipOnHover",
+  "tooltipProps",
+  "tooltipSubtitle",
+  "tooltipText",
+  "hover",
+];
+
+const disabledStyled = (props: RowExtraProps) => {
+  const colors = getColors(props);
+
+  return `
+    background-color: ${colors?.gray[100]};
+    color: ${colors?.gray[300]};
+  `;
+};
+
+const hoverStyled = (props: RowExtraProps) => {
+  const colors = getColors(props);
+
+  return `
+    &:hover {
+      background-color: ${colors?.gray[200]};
+    }
+  `;
+};
+
+export const StyledTableRow = styled("tr", {
+  shouldForwardProp: (prop) => !doNotForwardProps.includes(prop as string),
+})`
+  ${fontHeaderS}
+
+  ${(props: RowExtraProps) => {
+    const {
+      rowHeight,
+      useDivider = true,
+      selected = false,
+      disabled = false,
+      hover = true,
+    } = props;
+
+    const colors = getColors(props);
+
+    return `
+      display: flex;
+      align-items: center;
+      border-bottom: ${useDivider ? `solid 1px ${colors?.gray[300]}` : "none"};
+
+      ${rowHeight ? `max-height: ${rowHeight}px;` : ""}
+
+      ${selected ? `background-color: ${colors?.primary[200]};` : ""}
+
+      ${hover && hoverStyled(props)};
+
+      ${disabled && disabledStyled(props)}
+    `;
+  }}
+`;
