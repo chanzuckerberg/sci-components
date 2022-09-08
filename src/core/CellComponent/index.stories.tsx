84d3636918a9354f18362b25df1856e18a26fa1f
bethbertozzi <36094225+bethbertozzi@users.noreply.github.com>
2022-09-08 13:32:36 -0400

    feat(component cell): refined implementation and wrote tests (#244)
    * feat(component cell): wrote basic code
    
    * feat(component cell): refined implementation and wrote tests
    
    * style(component cell): fix order of object keys
    
    * test(component cell): updated snapshot
    
    * style(component cell): removed borders
    
    * fix(cell component): renamed to cellcomponent and fixed user story/test
    
    * test(cellcomponent): fix storybook control and update ss
    
    * test(cellcomponent): added test to check for child components
    
    * test(cell component): changed test to meet accessibility reqs
    
    * fix(cellcomponent): changed to accept only one child
    
    * feat(cellcomponent): added vertical align
    
    * docs(cell component): changed size of cells in stories
    
    * test(cell component): updated snapshot
    
    * refactor(cell component): change default to use icon instead of checkbox

---
+++ src/core/CellComponent/index.stories.tsx
@@ -0,0 +1,150 @@
+import { FormControlLabel, RadioGroup } from "@mui/material";
+import { Args, Story } from "@storybook/react";
+import * as React from "react";
+import Icon from "../Icon";
+import RadioButton from "../Radio";
+import TableRow from "../TableRow";
+import Tag from "../Tag";
+import CellComponentRaw from "./index";
+import { StyledStoryBody, StyledStoryHeading } from "./style";
+
+const CellComponent = (props: Args): JSX.Element => {
+  const { horizontalAlign, verticalAlign } = props;
+
+  return (
+    <table>
+      <tbody>
+        <TableRow
+          style={{
+            display: "block",
+            maxWidth: "280px",
+          }}
+        >
+          <CellComponentRaw
+            text-align={horizontalAlign}
+            verticalAlign={verticalAlign}
+            data-testid="CellComponent"
+            {...props}
+          />
+        </TableRow>
+      </tbody>
+    </table>
+  );
+};
+
+export default {
+  argTypes: {
+    horizontalAlign: {
+      control: { type: "select" },
+      options: ["left", "center", "right"],
+    },
+    verticalAlign: {
+      control: { type: "select" },
+      options: ["top", "center", "bottom"],
+    },
+  },
+  component: CellComponent,
+  title: "CellComponent",
+};
+
+const Template: Story = (props: Args) => {
+  const { horizontalAlign, verticalAlign } = props;
+
+  return (
+    <CellComponent
+      horizontalAlign={horizontalAlign}
+      verticalAlign={verticalAlign}
+    >
+      <Icon sdsSize="xl" sdsIcon="checkCircle" sdsType="static" />
+    </CellComponent>
+  );
+};
+
+export const Default = Template.bind({});
+
+Default.parameters = {
+  snapshot: {
+    skip: true,
+  },
+};
+
+Default.args = {
+  horizontalAlign: "center",
+  verticalAlign: "center",
+};
+
+const TestDemo = (props: Args): JSX.Element => {
+  const placementStyles = {
+    display: "grid",
+    gridColumnGap: "14px",
+    gridRowGap: "0px",
+    gridTemplateColumns: "112px 50px",
+    gridTemplateRows: "1fr",
+  };
+
+  return (
+    <div style={placementStyles as React.CSSProperties}>
+      <div>
+        <CellComponent
+          data-testid="CellComponentA"
+          horizontalAlign="right"
+          verticalAlign="bottom"
+        >
+          <div style={{ display: "block" }}>
+            <StyledStoryHeading data-testid="Child">
+              Lorem ipsum dolor
+            </StyledStoryHeading>
+            <StyledStoryBody data-testid="Child" style={{ paddingBottom: 10 }}>
+              Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor.
+            </StyledStoryBody>
+            <Tag
+              color="primary"
+              data-testid="Child"
+              label="Tertiary"
+              sdsStyle="rounded"
+              sdsType="primary"
+              {...props}
+            />
+          </div>
+        </CellComponent>
+      </div>
+      <div>
+        <CellComponent
+          data-testid="CellComponentB"
+          horizontalAlign="left"
+          verticalAlign="top"
+        >
+          <div style={{ display: "block" }}>
+            <StyledStoryHeading
+              data-testid="Child"
+              style={{ paddingBottom: 10 }}
+            >
+              Lorem ipsum dolor
+            </StyledStoryHeading>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              data-testid="Child"
+              defaultValue="demo"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                control={<RadioButton stage="checked" {...props} />}
+                label="Label"
+                value="demo"
+              />
+            </RadioGroup>
+          </div>
+        </CellComponent>
+      </div>
+    </div>
+  );
+};
+
+const TestTemplate: Story = (args) => <TestDemo {...args} />;
+
+export const Test = TestTemplate.bind({});
+
+Test.parameters = {
+  controls: { exclude: ["verticalAlign", "horizontalAlign"] },
+};
---
+++ src/core/CellComponent/index.tsx
@@ -0,0 +1,21 @@
+import React, { forwardRef, ReactNode } from "react";
+import { CellComponentExtraProps, StyledCellComponentData } from "./style";
+
+export interface CellComponentRawProps {
+  children?: ReactNode;
+}
+
+export type CellComponentProps = CellComponentRawProps &
+  CellComponentExtraProps;
+
+const CellComponent = forwardRef(
+  (props: CellComponentProps, _): JSX.Element | null => {
+    const { children } = props;
+
+    return (
+      <StyledCellComponentData {...props}>{children}</StyledCellComponentData>
+    );
+  }
+);
+
+export default CellComponent;
