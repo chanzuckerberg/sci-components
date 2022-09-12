dc0f27f3e68344307a8635a84b11f68ee907b53c
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2022-09-12 16:19:27 -0400

    fix(icon): change iconBank style

--- src/core/Icon/index.stories.tsx
+++ src/core/Icon/index.stories.tsx
@@ -1,13 +1,12 @@
 import styled from "@emotion/styled";
 import { Args, Story } from "@storybook/react";
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import {
   CommonThemeProps,
   getColors,
   getSpaces,
   getTypography,
 } from "../styles";
-import TooltipCondensed from "../TooltipCondensed";
 import Icon from "./index";
 import { iconMap, IconNameToSizes } from "./map";
 
@@ -78,79 +77,132 @@ const IconBankWrapper = styled("div")`
   ${(props: CommonThemeProps) => {
     const typography = getTypography(props);
     const spacings = getSpaces(props);
-    const colors = getColors(props);
 
     return `
       font-family: ${typography?.fontFamily};
       display: grid;
       grid-gap: ${spacings?.s}px;
       margin: 0 auto;
-      grid-template-columns: repeat(auto-fit, 180px);
-
-      input {
-        margin: ${spacings?.m}px 0 0 0;
-        border: none;
-        outline: none;
-        text-align: center;
-        font-weight: bold;
-        font-size: 14px;
-      }
-
-      span {
-        color: ${colors?.gray[400]};
-        font-size: 12px;
-      }
+      grid-template-columns: repeat(auto-fit, 150px);
     `;
   }}
 `;
 
 const IconWrapper = styled("div")`
-  align-items: center;
-  border: 1px dashed #eee;
-  border-radius: 2px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 8px;
+  ${(props: CommonThemeProps) => {
+    const colors = getColors(props);
+    const spacings = getSpaces(props);
+
+    return `
+      align-items: center;
+      border: 1px solid #eee;
+      border-radius: 2px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding: 8px;
+      position: relative;
+      cursor: pointer;
+      transition: all .2s;
+
+      p {
+        margin: ${spacings?.m}px 0 0 0;
+        text-align: center;
+        font-size: 13px;
+        background-color: transparent;
+      }
+
+      span {
+        color: ${colors?.gray[400]};
+        font-size: 11px;
+      }
+
+      &:hover {
+        border-radius: 2px;
+        background-color: ${colors?.primary[400]};
+        border-color: ${colors?.primary[400]};
+        color: white;
+
+        p {
+          color: white;
+        }
+
+        span {
+          color: white;
+        }
+
+        svg {
+          fill: white;
+        }
+
+        div.notif {
+          display: flex;
+        }
+      }
+
+      div.notif {
+        flex-direction: column;
+        border-radius: 2px;
+        display: none;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.3);
+        backdrop-filter: blur(3px);
+        color: white;
+        align-items: center;
+        justify-content: center;
+        p {
+          margin: 4px 0;
+        }
+      }
+    `;
+  }}
 `;
 
 export const IconBank = () => {
   const icons = Object.entries(iconMap);
-
-  const copyIconNameHandler = (
-    iconName: string,
-    icon: HTMLInputElement | null
-  ) => {
-    navigator.clipboard.writeText(iconName);
-    if (icon) icon.select();
-  };
 
   return (
     <IconBankWrapper>
       {icons.map(([sdsIcon, icon]) => {
         const { largeIcon } = icon;
         const sdsSize = largeIcon ? "l" : "s";
-        const iconInputRef = useRef<HTMLInputElement>(null);
+
+        const [copied, setCopied] = useState(false);
+
+        const copyIconNameHandler = (iconName: string) => {
+          navigator.clipboard.writeText(iconName);
+          setCopied(true);
+
+          setTimeout(() => {
+            setCopied(false);
+          }, 1500);
+        };
 
         return (
-          <TooltipCondensed title="Click to copy!" key={sdsIcon}>
-            <IconWrapper
-              onClick={() => copyIconNameHandler(sdsIcon, iconInputRef.current)}
-            >
-              <Icon
-                sdsSize={sdsSize}
-                sdsIcon={sdsIcon as keyof IconNameToSizes}
-                sdsType="static"
-              />
-              <input
-                type="text"
-                ref={iconInputRef}
-                value={sdsIcon}
-                onChange={() => {}}
-              />
-              <span>(size {sdsSize})</span>
-            </IconWrapper>
-          </TooltipCondensed>
+          <IconWrapper
+            key={sdsIcon}
+            onClick={() => copyIconNameHandler(sdsIcon)}
+          >
+            <Icon
+              sdsSize={sdsSize}
+              sdsIcon={sdsIcon as keyof IconNameToSizes}
+              sdsType="static"
+            />
+            <p>{sdsIcon}</p>
+            <span>(size {sdsSize})</span>
+            {copied && (
+              <div className="notif">
+                <Icon
+                  sdsSize={sdsSize}
+                  sdsIcon={sdsIcon as keyof IconNameToSizes}
+                  sdsType="static"
+                />
+                <p>Copied!</p>
+              </div>
+            )}
+          </IconWrapper>
         );
       })}
     </IconBankWrapper>
