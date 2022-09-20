d129da41a6a178b344f46ce243fd03314d7469e9
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2022-09-20 12:00:22 -0400

    feat(tableheader): add tr tag to the tableheader

--- src/core/TableHeader/index.stories.tsx
+++ src/core/TableHeader/index.stories.tsx
@@ -2,18 +2,15 @@
 import { Args, Story } from "@storybook/react";
 import * as React from "react";
 import CellHeader from "../CellHeader";
-import TableRow from "../TableRow";
 import TableHeaderRaw from "./index";
 
 const TableHeader = (props: Args): JSX.Element => {
   return (
     <table>
       <TableHeaderRaw {...props}>
-        <TableRow hover={false} shouldShowTooltipOnHover={false}>
-          <CellHeader active>Column 1</CellHeader>
-          <CellHeader>Column 2</CellHeader>
-          <CellHeader>Column 3</CellHeader>
-        </TableRow>
+        <CellHeader active>Column 1</CellHeader>
+        <CellHeader>Column 2</CellHeader>
+        <CellHeader>Column 3</CellHeader>
       </TableHeaderRaw>
     </table>
   );
@@ -37,11 +34,9 @@ Default.parameters = {
 const TestDemo = (): JSX.Element => (
   <table>
     <TableHeaderRaw data-testid="TableHeader">
-      <TableRow hover={false} shouldShowTooltipOnHover={false}>
-        <CellHeader active>Column 1</CellHeader>
-        <CellHeader>Column 2</CellHeader>
-        <CellHeader>Column 3</CellHeader>
-      </TableRow>
+      <CellHeader active>Column 1</CellHeader>
+      <CellHeader>Column 2</CellHeader>
+      <CellHeader>Column 3</CellHeader>
     </TableHeaderRaw>
   </table>
 );
