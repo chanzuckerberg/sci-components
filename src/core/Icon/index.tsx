8fd01bfc5f1d74be0e0ff28f180942bcb0dfc5f4
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2023-02-18 18:04:13 -0500

    feat(icon): add color prop to the icon component (#358)
    * feat(icon): add color prop to the icon component
    
    * fix(icon): fix color override
    
    * refactor(icon): fix icon styles
    
    * feat(icon): add color shade to the icon cmoponent

--- src/core/Icon/index.tsx
+++ src/core/Icon/index.tsx
@@ -1,17 +1,27 @@
 import React, { ForwardedRef, forwardRef } from "react";
 import { iconMap, IconNameToSizes } from "./map";
-import { IconExtraProps, StyledIcon, StyledSvgIcon } from "./style";
+import {
+  IconExtraProps,
+  SdsIconColorType,
+  StyledIcon,
+  StyledSvgIcon,
+} from "./style";
 
 export type { IconNameToSizes };
 
+interface SdsIconWithColor {
+  color?: SdsIconColorType;
+  shade?: 100 | 200 | 300 | 400 | 500 | 600;
+}
+
 export type IconProps<IconName extends keyof IconNameToSizes> =
-  IconExtraProps<IconName>;
+  IconExtraProps<IconName> & SdsIconWithColor;
 
 /**
  * @see https://mui.com/material-ui/icons/#icons
  */
 const Icon = forwardRef(function Icon<IconName extends keyof IconNameToSizes>(
-  { className, sdsIcon, sdsSize, sdsType }: IconProps<IconName>,
+  { className, color, shade, sdsIcon, sdsSize, sdsType }: IconProps<IconName>,
   ref: ForwardedRef<HTMLDivElement | null>
 ): JSX.Element | null {
   const icon = iconMap[sdsIcon] ?? {};
@@ -21,6 +31,8 @@ const Icon = forwardRef(function Icon<IconName extends keyof IconNameToSizes>(
     return (
       <StyledIcon ref={ref}>
         <StyledSvgIcon
+          iconColor={color}
+          shade={shade}
           className={className}
           fillcontrast="white"
           viewBox="0 0 14 14"
@@ -36,6 +48,8 @@ const Icon = forwardRef(function Icon<IconName extends keyof IconNameToSizes>(
     return (
       <StyledIcon ref={ref}>
         <StyledSvgIcon
+          iconColor={color}
+          shade={shade}
           className={className}
           fillcontrast="white"
           viewBox="0 0 22 22"
