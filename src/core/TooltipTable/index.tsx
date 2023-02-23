d7b2c29c883b603cddc089461893a332f90b8705
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2023-02-23 12:47:25 -0500

    fix(tooltiptable): fix non-unique react keys

--- src/core/TooltipTable/index.tsx
+++ src/core/TooltipTable/index.tsx
@@ -22,15 +22,18 @@ const TooltipTableContent = (props: TooltipTableContentProps): JSX.Element => {
   return (
     <TableContainer {...rest}>
       {contentAlert && <Alert>{contentAlert}</Alert>}
-      {data?.map((section) => (
-        <Section disabled={section.disabled} key={`${section.label}`}>
+      {data?.map((section, index) => (
+        <Section
+          disabled={section.disabled}
+          key={`${section.label?.concat(index.toString())}`}
+        >
           <SectionLabel disabled={section.disabled} label={section.label}>
             {section.label}
           </SectionLabel>
           <Table size="small">
             <TableBody>
-              {section.dataRows.map((row) => (
-                <TableRow key={row.label}>
+              {section.dataRows.map((row, rowIndex) => (
+                <TableRow key={row.label.concat(rowIndex.toString())}>
                   <RowLabel disabled={section.disabled} align="left">
                     {row.label}
                   </RowLabel>
