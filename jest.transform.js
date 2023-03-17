4f6cf72f17e8fe38e976e3f3167c4712feabe899
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2023-03-17 01:05:13 -0400

    feat(inputdropdown): revert @emotion/babel-plugin changes

--- jest.transform.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
-module.exports = require("babel-jest").createTransformer({
-  plugins: ["@emotion"],
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-    "@babel/react",
-  ],
-});
