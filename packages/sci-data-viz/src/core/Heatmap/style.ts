904a53f3231af15b2eccd1fa9045e253ed721141
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2023-04-13 02:37:26 -0400

    fix(namespace-check): fix github workflows

--- packages/sci-data-viz/src/core/Heatmap/style.ts
+++ packages/sci-data-viz/src/core/Heatmap/style.ts
@@ -3,11 +3,10 @@ import { styled } from "@mui/material/styles";
 export const StyledHeatmap = styled("div")`
   width: 100%;
   height: 100px;
-  background-color: red;
+  background-color: #212121;
   display: flex;
   justify-content: center;
   align-items: center;
   color: white;
   font-size: 1rem;
-  font-family: monospace;
 `;
