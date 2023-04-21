194b40588ac7db40ae10a0dfb76c0319c4046a28
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2023-04-21 15:28:54 -0400

    fix(heatmap): change bg color

--- packages/data-viz/src/core/Heatmap/style.ts
+++ packages/data-viz/src/core/Heatmap/style.ts
@@ -3,7 +3,7 @@ import { styled } from "@mui/material/styles";
 export const StyledHeatmap = styled("div")`
   width: 100%;
   height: 100px;
-  background-color: #212121;
+  background-color: forestgreen;
   display: flex;
   justify-content: center;
   align-items: center;
