3a5440c3a02ada7f22c5cc68be8456ba2d01f720
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2023-04-21 16:33:09 -0400

    fix(heatmap): fix color contrast

--- packages/data-viz/src/core/Heatmap/style.ts
+++ packages/data-viz/src/core/Heatmap/style.ts
@@ -3,7 +3,7 @@ import { styled } from "@mui/material/styles";
 export const StyledHeatmap = styled("div")`
   width: 100%;
   height: 100px;
-  background-color: forestgreen;
+  background-color: #212121;
   display: flex;
   justify-content: center;
   align-items: center;
