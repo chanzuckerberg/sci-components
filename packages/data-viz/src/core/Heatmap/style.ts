1f42e15a89ccd52777e5e11016e238133ae18af9
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2023-04-26 13:04:10 -0400

    fix(styles): return to previous styling

--- packages/data-viz/src/core/Heatmap/style.ts
+++ packages/data-viz/src/core/Heatmap/style.ts
@@ -3,7 +3,7 @@ import { styled } from "@mui/material/styles";
 export const StyledHeatmap = styled("div")`
   width: 100%;
   height: 100px;
-  background-color: #222;
+  background-color: #212121;
   display: flex;
   justify-content: center;
   align-items: center;
