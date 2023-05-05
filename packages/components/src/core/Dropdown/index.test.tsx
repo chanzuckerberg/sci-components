462dade60200badfd190627ee718deaea39f4e13
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2023-05-05 15:03:10 -0400

    feat(dropdown): new tests for the dropdown component

--- packages/components/src/core/Dropdown/index.test.tsx
+++ packages/components/src/core/Dropdown/index.test.tsx
@@ -8,16 +8,36 @@ import Meta, { Test as TestStory } from "./index.stories";
 
 const Test = composeStory(TestStory, Meta);
 
+const STATUS_CANT_FIND_TEXT = "Status: can't reproduce";
+
 describe("<Dropdown />", () => {
   generateSnapshots(snapshotTestStoryFile);
 
-  it("renders Dropdown component", () => {
+  it("Dropdown component renders correctly", () => {
     render(<Test {...Test.args} />);
     const DropdownElement = screen.getByTestId("dropdown");
     expect(DropdownElement).not.toBeNull();
   });
 
-  it("renders loading indicator when loading state passed in", () => {
+  it("Upon clicking the button, the dropdown menu closes.", () => {
+    const args = {
+      ...Test.args,
+    };
+
+    render(<Test {...args} />);
+
+    // Open the dropdown
+    const DropdownElement = screen.getByTestId("dropdown");
+    fireEvent.click(DropdownElement);
+
+    // Click on the button again to close it
+    fireEvent.click(DropdownElement);
+
+    const menuItem = screen.queryByText(STATUS_CANT_FIND_TEXT);
+    expect(menuItem).toEqual(null);
+  });
+
+  it("Prop Test (loeading): Loading indicator correctly renders when loading state is true", () => {
     const args = {
       ...Test.args,
       DropdownMenuProps: {
@@ -39,5 +59,64 @@ describe("<Dropdown />", () => {
 
     fireEvent.click(DropdownElement);
     expect(screen.getAllByText("Loading")).not.toBeNull();
+  });
+
+  it("Prop Test (search): Dropdown component search function correctly filters out Type items", () => {
+    const args = {
+      ...Test.args,
+      search: true,
+    };
+
+    render(<Test {...args} />);
+
+    // Open the dropdown
+    const DropdownElement = screen.getByTestId("dropdown");
+    fireEvent.click(DropdownElement);
+
+    // Select the search input by placeholder
+    const searchInput = screen.getByPlaceholderText("Search");
+    fireEvent.change(searchInput, { target: { value: "Status" } });
+
+    // We want to test that there are no items with "Type: *" in the dropdown menu
+    // because we have searched for Status
+    const typeBugItem = screen.queryByText("Type: bug");
+    expect(typeBugItem).toEqual(null);
+  });
+
+  it("Prop Test (Label): Dropdown component correctly renders with a new label", () => {
+    const args = {
+      ...Test.args,
+      label: "Test Label",
+    };
+
+    render(<Test {...args} />);
+    expect(screen.getAllByText("Test Label")).not.toBeNull();
+  });
+
+  it("Prop Test (closeOnBlur): Dropdown closes when button is clicked with closeOnBlue set to true", async () => {
+    const args = {
+      ...Test.args,
+      closeOnBlur: true,
+    };
+
+    render(<Test {...args} />);
+
+    // Open the dropdown
+    const DropdownElement = screen.getByTestId("dropdown");
+    fireEvent.click(DropdownElement);
+
+    // Awaiting to find the STATUS_CANT_FIND_TEXT on the screen to
+    // make sure that the dropdown is opened
+    await screen.findByText(STATUS_CANT_FIND_TEXT);
+
+    // Click on the button again to close it
+    fireEvent.click(DropdownElement);
+
+    // Awaiting to find the STATUS_CANT_FIND_TEXT on the screen to
+    // make sure that the dropdown is closed
+    await screen.findByText(STATUS_CANT_FIND_TEXT);
+
+    const menuItem = screen.queryByText(STATUS_CANT_FIND_TEXT);
+    expect(menuItem).toEqual(null);
   });
 });
