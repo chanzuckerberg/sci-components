bdb41f9a1e4581e45d9adeda7633593585f717e0
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2023-08-03 17:23:22 -0400

    feat(storybook): upgrade (#551)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+import { dirname, join } from "path";
 const path = require("path");
 const toPath = (filePath) => path.join(process.cwd(), filePath);
 module.exports = {
@@ -28,15 +29,15 @@ module.exports = {
     "../packages/data-viz/src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-    "@storybook/react",
-    "storybook-addon-pseudo-states",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/react"),
+    getAbsolutePath("storybook-addon-pseudo-states"),
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: getAbsolutePath("@storybook/react-webpack5"),
     options: {},
   },
   webpackFinal: async (config) => {
@@ -62,3 +63,10 @@ module.exports = {
     autodocs: false,
   },
 };
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
