f0f824ca95c609f5f0a5754e848834a9c9e02764
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2023-09-13 20:16:48 -0400

    fix(jest): new jest config

---
+++ .storybook/test-runner-jest.js
@@ -0,0 +1,13 @@
+const { getJestConfig } = require("@storybook/test-runner");
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
+module.exports = {
+  // The default configuration comes from @storybook/test-runner
+  ...getJestConfig(),
+  /** Add your own overrides below
+   * @see https://jestjs.io/docs/configuration
+   */
+  testTimeout: 30000,
+};
