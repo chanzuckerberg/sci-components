c2fc647d836ddf2fbd1a6204da9489e48d4fe2e1
Andrew Huth <ahuth@chanzuckerberg.com>
2023-11-29 11:20:19 -0500

    chore: remove the unused jest-canvas-mock

--- jest.common.config.js
+++ jest.common.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   moduleDirectories: ["node_modules", "<rootDir>"],
   preset: "ts-jest",
-  setupFilesAfterEnv: ["jest-canvas-mock", "./jest.setup.ts"],
+  setupFilesAfterEnv: ["./jest.setup.ts"],
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
