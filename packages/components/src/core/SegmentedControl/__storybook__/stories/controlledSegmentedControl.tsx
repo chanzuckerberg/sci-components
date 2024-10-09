d59af72ced81f4e4577aa85b6cd272a343170931
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2024-10-09 12:05:26 -0400

    feat(segmentedcontrol): add disabled state in button definition and controlled/uncontrolled state (#869)

---
+++ packages/components/src/core/SegmentedControl/__storybook__/stories/controlledSegmentedControl.tsx
@@ -0,0 +1,20 @@
+import { Args } from "@storybook/react";
+import { useState } from "react";
+import RawSegmentedControl from "src/core/SegmentedControl";
+
+export const ControlledSegmentedControlDemo = (props: Args): JSX.Element => {
+  const { buttonDefinition, ...rest } = props;
+
+  const [value, setValue] = useState("C");
+  return (
+    <RawSegmentedControl
+      buttonDefinition={buttonDefinition}
+      onChange={(_event, newValue) => {
+        console.log(newValue);
+        setValue(newValue);
+      }}
+      value={value}
+      {...rest}
+    />
+  );
+};
