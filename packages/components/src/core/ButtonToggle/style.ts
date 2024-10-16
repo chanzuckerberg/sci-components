3f532ad20c6b872990c5122d93774ab6c87ee667
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2024-10-16 12:08:10 -0400

    feat(ButtonToggle): implement new icon variant of toggle (#860)
    * feat(buttontoggle): first implementation of the ButtonToggle component
    
    * test(buttontoggle): added tests
    
    * fix(buttontoggle): fix A11y errors
    
    * refactor(buttontoggle): fix styling bugs in CellBasic

---
+++ packages/components/src/core/ButtonToggle/style.ts
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+import ButtonIcon from "../ButtonIcon";
+import { CommonThemeProps, getSemanticColors } from "../styles";
+import { ButtonToggleProps } from ".";
+
+/**
+ * (masoudmanson): Since StyledButtonToggle is built on top of ButtonIcon,
+ * we only need to exclude the `sdsStage` prop from being forwarded,
+ * as it is not a valid prop for ButtonIcon.
+ * All other props should be passed down to ButtonIcon; otherwise,
+ * the component won’t function as expected.
+ */
+const doNotForwardProps = ["sdsStage"];
+
+interface ButtonToggleExtraProps extends CommonThemeProps {
+  sdsStage?: ButtonToggleProps["sdsStage"];
+}
+
+export const StyledButtonToggle = styled(ButtonIcon, {
+  shouldForwardProp: (prop: string) => !doNotForwardProps.includes(prop),
+})`
+  ${(props: ButtonToggleExtraProps) => {
+    const { sdsStage } = props;
+    const semanticColors = getSemanticColors(props);
+
+    return `
+      background-color: ${sdsStage === "on" ? semanticColors?.base?.fillHover : "transparent"};
+    `;
+  }}
+`;
