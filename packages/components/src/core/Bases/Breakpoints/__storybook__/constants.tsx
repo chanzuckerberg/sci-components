a809c018e3b7de62c457db6c21047b256ed6f959
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2024-11-14 02:43:44 -0500

    feat(breakpoints): added new SDS breakpoints (#888)
    * feat(breakpoints): added new sds breakpoints
    
    * style(dropdown): fix the modal styles in dropdown demo

---
+++ packages/components/src/core/Bases/Breakpoints/__storybook__/constants.tsx
@@ -0,0 +1,7 @@
+import { Breakpoint } from "@mui/material";
+
+export const BREAKPOINTS_USAGE: Record<Breakpoint, string> = {
+  lg: "Large breakpoint can be used for all large displays ( > 1024px).",
+  md: "Medium breakpoint will work for small laptops and large-screened tablets (512px - 1023px).",
+  sm: "Small breakpoint will work for mobile devices and small-screened tablets (0px - 511px).",
+};
