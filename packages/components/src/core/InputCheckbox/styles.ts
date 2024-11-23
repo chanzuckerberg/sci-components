2f6cd4a2ee662a7002a75a1344d54c0b0322ec9a
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2024-11-23 00:32:26 -0500

    refactor(inputcheckbox): refactored the InputCheckbox and InputRadio icons

--- packages/components/src/core/InputCheckbox/styles.ts
+++ packages/components/src/core/InputCheckbox/styles.ts
@@ -7,11 +7,13 @@ import styled from "@emotion/styled";
 import { focusVisibleA11yStyle } from "src/core/styles/common/mixins/a11y";
 import {
   CommonThemeProps,
+  getBorders,
   getIconSizes,
   getSemanticColors,
   getSpaces,
 } from "src/core/styles";
 import { SDSPalette, fontBodyXs, fontBodyXxs } from "../styles";
+import Icon from "../Icon";
 
 export interface CheckboxExtraProps extends CommonThemeProps {
   intent?: "default" | "negative" | "notice" | "positive";
@@ -25,88 +27,68 @@ const intentToColor = {
   positive: "positive",
 };
 
-export const StyledCheckbox = styled(RawCheckbox)`
+export const StyledIcon = styled(Icon)`
+  ${(props: CheckboxExtraProps) => {
+    const iconSizes = getIconSizes(props);
+
+    return `
+      height: ${iconSizes?.xs.height}px;
+      width: ${iconSizes?.xs.width}px;
+    `;
+  }}
+`;
+
+export const StyledCheckboxDefaultIcon = styled("span")`
   ${(props: CheckboxExtraProps) => {
     const { intent = "default" } = props;
 
     const iconSizes = getIconSizes(props);
-    const spaces = getSpaces(props);
-    const semanticColors = getSemanticColors(props);
+    const borders = getBorders(props);
 
-    const checkboxColor = intentToColor[intent] as keyof SDSPalette;
+    const borderColor = intentToColor[intent] as keyof SDSPalette;
 
     return `
-      color: ${semanticColors?.[checkboxColor]?.border};
+      height: ${iconSizes?.s.height}px;
+      width: ${iconSizes?.s.width}px;
+      border: ${borders?.[borderColor]?.default};
+      border-radius: 2px;
+    `;
+  }}
+`;
 
-      &:hover {
-        color: ${semanticColors?.base?.borderHover};
-        background-color: transparent;
-      }
+export const StyledCheckboxCheckedIcon = styled("div")`
+  ${(props: CheckboxExtraProps) => {
+    const iconSizes = getIconSizes(props);
+    const borders = getBorders(props);
+    const semanticColors = getSemanticColors(props);
 
-      &.${checkboxClasses.disabled} {
-        color: ${semanticColors?.base?.borderDisabled};
-      }
+    return `
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: ${iconSizes?.s.height}px;
+      width: ${iconSizes?.s.width}px;
+      border: ${borders?.accent?.default};
+      border-radius: 2px;
+      background-color: ${semanticColors?.accent?.fillPrimary};
 
-      &.${checkboxClasses.checked} {
-        color: ${semanticColors?.accent?.border};
-
-        &:hover {
-          color: ${semanticColors?.accent?.borderHover};
-          background-color: transparent;
-        }
-
-        &.${checkboxClasses.disabled} {
-          color: ${semanticColors?.base?.borderDisabled};
-        }
-      }
-
-      &.${checkboxClasses.root} {
-        ${focusVisibleA11yStyle(props)}
-        padding: 0;
-        margin-right: ${spaces?.s}px;
-        border-radius: 0;
-      }
-
-      .MuiSvgIcon-root {
-        height: ${iconSizes?.s.height}px;
-        width: ${iconSizes?.s.width}px;
+      ${StyledIcon} {
+        fill: ${semanticColors?.base?.ornamentPrimaryInverse};
       }
     `;
   }}
 `;
 
-export const StyledFormControlLabel = styled(FormControlLabel)`
+export const StyledCheckbox = styled(RawCheckbox)`
   ${(props: CheckboxExtraProps) => {
-    const { disabled } = props;
-
     const spaces = getSpaces(props);
-    const semanticColors = getSemanticColors(props);
 
     return `
-      align-items: start;
-      margin-bottom: ${spaces?.l}px;
-      margin-left: 0;
-      margin-right: 0;
-      width: fit-content;
-      user-select: ${disabled ? "none" : "auto"};
-
-      &:hover {
-        ${StyledCheckbox} {
-          color: ${semanticColors?.base?.borderHover};
-
-          &.${checkboxClasses.disabled} {
-            color: ${semanticColors?.base?.borderDisabled};
-          }
-
-          &.${checkboxClasses.checked} {
-            color: ${semanticColors?.accent?.borderHover};
-            background-color: transparent;
-
-            &.${checkboxClasses.disabled} {
-              color: ${semanticColors?.base?.borderDisabled};
-            }
-          }
-        }
+      &.${checkboxClasses.root} {
+        ${focusVisibleA11yStyle(props)}
+        padding: 0;
+        margin-right: ${spaces?.s}px;
+        border-radius: 0;
       }
     `;
   }}
@@ -134,11 +116,85 @@ export const StyledCheckboxCaption = styled("span")`
   ${fontBodyXxs}
 
   ${(props: CheckboxExtraProps) => {
-    const { disabled } = props;
     const semanticColors = getSemanticColors(props);
 
     return `
-      color: ${disabled ? semanticColors?.base?.textDisabled : semanticColors?.base?.textSecondary};
+      color: ${semanticColors?.base?.textSecondary};
+    `;
+  }}
+`;
+
+const disabledStyles = (props: CheckboxExtraProps) => {
+  const semanticColors = getSemanticColors(props);
+  const borders = getBorders(props);
+
+  return `
+    user-select: none;
+
+    ${StyledCheckboxDefaultIcon} {
+      border: ${borders?.base?.disabled};
+    }
+
+    ${StyledCheckboxCheckedIcon} {
+      border: ${borders?.base?.disabled};
+      background-color: ${semanticColors?.base?.ornamentDisabled};
+    }
+
+    ${StyledCheckboxCaption} {
+      color: ${semanticColors?.base?.textDisabled};
+    }
+
+    &:hover, &:active {
+      ${StyledCheckboxDefaultIcon} {
+        border: ${borders?.base?.disabled};
+      }
+
+      ${StyledCheckboxCheckedIcon} {
+        border: ${borders?.base?.disabled};
+        background-color: ${semanticColors?.base?.ornamentDisabled};
+      }
+    }
+  `;
+};
+
+export const StyledFormControlLabel = styled(FormControlLabel)`
+  ${(props: CheckboxExtraProps) => {
+    const { disabled } = props;
+
+    const spaces = getSpaces(props);
+    const semanticColors = getSemanticColors(props);
+    const borders = getBorders(props);
+
+    return `
+      align-items: start;
+      margin-bottom: ${spaces?.l}px;
+      margin-left: 0;
+      margin-right: 0;
+      width: fit-content;
+
+      &:hover {
+        ${StyledCheckboxDefaultIcon} {
+          border: ${borders?.base?.hover};
+        }
+
+        ${StyledCheckboxCheckedIcon} {
+          border: ${borders?.accent?.hover};
+          background-color: ${semanticColors?.accent?.fillHover};
+        }
+      }
+
+      &:active {
+        ${StyledCheckboxDefaultIcon} {
+          border: ${borders?.base?.pressed};
+        }
+
+        ${StyledCheckboxCheckedIcon} {
+          border: ${borders?.accent?.pressed};
+          background-color: ${semanticColors?.accent?.fillPressed};
+        }
+      }
+
+      ${disabled && disabledStyles(props)}
     `;
   }}
 `;
