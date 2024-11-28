96fbc0150f2d9820ec04bf5f10663f2d43267e0b
Jeremy <jeremyasuncion808@gmail.com>
2024-11-27 23:36:56 -0800

    fix(navigationfooter): address styling issues

--- packages/components/src/core/NavigationFooter/index.stories.tsx
+++ packages/components/src/core/NavigationFooter/index.stories.tsx
@@ -4,6 +4,8 @@ import NavigationFooter, {
   NavigationFooterProps,
 } from "./index";
 import { ReactNode } from "react";
+import { getSemanticColors } from "../styles";
+import { useTheme } from "@mui/material";
 
 function ExampleLogo({
   children,
@@ -14,13 +16,18 @@ function ExampleLogo({
   height: number;
   width: number;
 }) {
+  const theme = useTheme();
+  const colors = getSemanticColors({ theme });
+
   return (
     <div
       style={{
         alignItems: "center",
-        border: "1px dashed black",
+        border: `1px dashed ${colors?.base.border}`,
+        color: colors?.base.textPrimary,
         display: "flex",
         fontSize: 10,
+        fontWeight: "normal",
         height,
         justifyContent: "center",
         whiteSpace: "nowrap",
@@ -35,17 +42,32 @@ function ExampleLogo({
 export default {
   args: {
     images: [
-      <ExampleLogo key="1" width={100} height={40}>
-        Image Slot
-      </ExampleLogo>,
+      {
+        image: (
+          <ExampleLogo width={100} height={40}>
+            Image Slot
+          </ExampleLogo>
+        ),
+        url: "https://example.com/1",
+      },
 
-      <ExampleLogo key="2" width={100} height={40}>
-        Image Slot
-      </ExampleLogo>,
+      {
+        image: (
+          <ExampleLogo width={100} height={40}>
+            Image Slot
+          </ExampleLogo>
+        ),
+        url: "https://example.com/2",
+      },
 
-      <ExampleLogo key="3" width={100} height={40}>
-        Image Slot
-      </ExampleLogo>,
+      {
+        image: (
+          <ExampleLogo width={100} height={40}>
+            Image Slot
+          </ExampleLogo>
+        ),
+        url: "https://example.com/3",
+      },
     ],
 
     logo: (
@@ -53,6 +75,8 @@ export default {
         Logo Slot
       </ExampleLogo>
     ),
+
+    logoUrl: "https://example.com",
 
     navItems: Array.from(Array(5)).map<NavigationFooterNavItem>((_, idx) => ({
       label: `Nav Item ${idx + 1}`,
@@ -64,7 +88,7 @@ export default {
       url: `https://example.com/nav/${idx + 1}`,
     })),
 
-    tag: "BETA",
+    tag: "Beta",
     tagColor: "beta",
     title: "Logo Name",
   } satisfies NavigationFooterProps,
