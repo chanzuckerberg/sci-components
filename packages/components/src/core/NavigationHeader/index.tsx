5fe875c2644a406ec8579089143ba5f3ca72f245
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2025-01-27 18:55:17 -0500

    fix(navigationheader): fixed Button and ButtonDropdown types
    Added tests and coverred all available controls in the storybook for the NavigationHeader component

--- packages/components/src/core/NavigationHeader/index.tsx
+++ packages/components/src/core/NavigationHeader/index.tsx
@@ -1,6 +1,6 @@
 import { useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import { ReactNode, useState } from "react";
+import { forwardRef, ReactNode, useState } from "react";
 import { SdsTagColorType } from "src/core/Tag";
 import {
   StyledButtonSection,
@@ -47,115 +47,209 @@ export interface NavigationHeaderProps<T extends string = string> {
   title: string;
 }
 
-export default function NavigationHeader<T extends string = string>({
-  activePrimaryNavKey,
-  buttons,
-  hasInvertedStyle,
-  logo,
-  logoUrl,
-  primaryNavItems,
-  primaryNavPosition = "left",
-  searchProps,
-  secondaryNavItems,
-  setActivePrimaryNavKey,
-  showSearch = true,
-  tag,
-  tagColor,
-  title,
-}: NavigationHeaderProps<T>) {
-  const theme = useTheme();
-  const mode = theme?.palette?.mode || "light";
+const NavigationHeader = forwardRef<HTMLDivElement, NavigationHeaderProps>(
+  <T extends string = string>(
+    props: NavigationHeaderProps<T>,
+    ref: React.Ref<HTMLDivElement>
+  ) => {
+    const {
+      activePrimaryNavKey,
+      buttons,
+      hasInvertedStyle,
+      logo,
+      logoUrl,
+      primaryNavItems,
+      primaryNavPosition = "left",
+      searchProps,
+      secondaryNavItems,
+      setActivePrimaryNavKey,
+      showSearch = true,
+      tag,
+      tagColor,
+      title,
+    } = props;
 
-  const isNarrow = useMediaQuery(theme.breakpoints.down("md"));
-  const [drawerOpen, setDrawerOpen] = useState(true);
+    const theme = useTheme();
+    const mode = theme?.palette?.mode || "light";
 
-  const search = showSearch && (
-    <StyledSearch
-      id="nav-bar-search"
-      label="Search"
-      placeholder="Search"
-      sdsStyle="rounded"
-      customTheme={
-        hasInvertedStyle ? (mode === "light" ? "dark" : "light") : "auto"
+    const isNarrow = useMediaQuery(theme.breakpoints.down("md"));
+    const [drawerOpen, setDrawerOpen] = useState(true);
+
+    const renderSearch = () => {
+      return (
+        showSearch && (
+          <StyledSearch
+            id="nav-bar-search"
+            label="Search"
+            placeholder="Search"
+            sdsStyle="rounded"
+            customTheme={
+              hasInvertedStyle ? (mode === "light" ? "dark" : "light") : "auto"
+            }
+            hasInvertedStyle={hasInvertedStyle}
+            {...searchProps}
+          />
+        )
+      );
+    };
+
+    const renderLogoNode = () => {
+      let logoNode = (
+        <StyledTitleContainer hasInvertedStyle={hasInvertedStyle}>
+          <StyledLogoWrapper>{logo}</StyledLogoWrapper>
+
+          <p>{title}</p>
+
+          {tag && <StyledTag color={tagColor} label={tag} hover={false} />}
+        </StyledTitleContainer>
+      );
+
+      if (logoUrl) {
+        logoNode = (
+          <StyledLogoLinkWrapper href={logoUrl}>
+            {logoNode}
+          </StyledLogoLinkWrapper>
+        );
       }
-      hasInvertedStyle={hasInvertedStyle}
-      {...searchProps}
-    />
-  );
 
-  let logoNode = (
-    <StyledTitleContainer hasInvertedStyle={hasInvertedStyle}>
-      <StyledLogoWrapper>{logo}</StyledLogoWrapper>
+      return logoNode;
+    };
 
-      <p>{title}</p>
+    const renderPrimaryNav = () => {
+      return (
+        activePrimaryNavKey &&
+        setActivePrimaryNavKey &&
+        primaryNavItems &&
+        primaryNavItems.length > 0 && (
+          <NavigationHeaderPrimaryNav
+            items={primaryNavItems}
+            value={activePrimaryNavKey}
+            onChange={setActivePrimaryNavKey}
+            hasInvertedStyle={hasInvertedStyle}
+          />
+        )
+      );
+    };
 
-      {tag && <StyledTag color={tagColor} label={tag} hover={false} />}
-    </StyledTitleContainer>
-  );
+    const renderSecondaryNav = () => {
+      return (
+        secondaryNavItems &&
+        secondaryNavItems.length > 0 && (
+          <NavigationHeaderSecondaryNav
+            items={secondaryNavItems}
+            hasInverseStyle={hasInvertedStyle}
+          />
+        )
+      );
+    };
 
-  if (logoUrl) {
-    logoNode = (
-      <StyledLogoLinkWrapper href={logoUrl}>{logoNode}</StyledLogoLinkWrapper>
-    );
-  }
+    const renderButtonsNode = () => {
+      return (
+        buttons &&
+        buttons.length > 0 && (
+          <StyledButtonSection>
+            {buttons.map((buttonProps, idx) => {
+              const isIconButton = "icon" in buttonProps;
+              const key = `button-${idx}`;
+              const fullWidth = isNarrow ? { width: "100%" } : undefined;
 
-  const primaryNav = activePrimaryNavKey &&
-    setActivePrimaryNavKey &&
-    primaryNavItems &&
-    primaryNavItems.length > 0 && (
-      <NavigationHeaderPrimaryNav
-        items={primaryNavItems}
-        value={activePrimaryNavKey}
-        onChange={setActivePrimaryNavKey}
-        hasInvertedStyle={hasInvertedStyle}
-      />
-    );
-
-  const secondaryNav = secondaryNavItems && secondaryNavItems.length > 0 && (
-    <NavigationHeaderSecondaryNav
-      items={secondaryNavItems}
-      hasInverseStyle={hasInvertedStyle}
-    />
-  );
-
-  const buttonsNode = buttons && buttons.length > 0 && (
-    <StyledButtonSection>
-      {buttons.map((buttonProps, idx) => {
-        const isIconButton = "icon" in buttonProps;
-        const key = `button-${idx}`;
-        const fullWidth = isNarrow ? { width: "100%" } : undefined;
-
-        if (isIconButton && isNarrow && buttonProps.icon) {
-          return (
-            <Button
-              key={key}
-              sx={fullWidth}
-              sdsStyle="minimal"
-              sdsType="secondary"
-              isAllCaps={false}
-              startIcon={<Icon sdsSize="s" sdsIcon="Person" />}
-              customTheme={
-                hasInvertedStyle
-                  ? mode === "light"
-                    ? "dark"
-                    : "light"
-                  : "auto"
+              if (isIconButton && isNarrow && buttonProps.icon) {
+                return (
+                  <Button
+                    key={key}
+                    sx={fullWidth}
+                    sdsStyle="minimal"
+                    sdsType="secondary"
+                    isAllCaps={false}
+                    startIcon={<Icon sdsSize="s" sdsIcon="Person" />}
+                    customTheme={
+                      hasInvertedStyle
+                        ? mode === "light"
+                          ? "dark"
+                          : "light"
+                        : "auto"
+                    }
+                  >
+                    {buttonProps.children}
+                  </Button>
+                );
               }
-            >
-              {buttonProps.children}
-            </Button>
-          );
-        }
 
-        if (isIconButton && !isNarrow) {
-          return (
-            <Button
-              key={key}
-              sdsIconProps={{ sdsSize: "l" }}
-              sdsStyle="icon"
+              if (isIconButton && !isNarrow) {
+                return (
+                  <Button
+                    key={key}
+                    sdsIconProps={{ sdsSize: "l" }}
+                    sdsStyle="icon"
+                    sdsType="secondary"
+                    icon={buttonProps.icon}
+                    aria-label={String(buttonProps.children)}
+                    customTheme={
+                      hasInvertedStyle
+                        ? mode === "light"
+                          ? "dark"
+                          : "light"
+                        : "auto"
+                    }
+                  />
+                );
+              }
+
+              return (
+                <StyledHeaderButton
+                  key={key}
+                  sx={fullWidth}
+                  sdsStyle="rounded"
+                  {...(buttonProps as SdsMinimalButtonProps | SdsButtonProps)}
+                  hasInvertedStyle={hasInvertedStyle}
+                />
+              );
+            })}
+          </StyledButtonSection>
+        )
+      );
+    };
+
+    const renderHeaderContent = () => {
+      const logoNode = renderLogoNode();
+      const search = renderSearch();
+      const primaryNav = renderPrimaryNav();
+      const secondaryNav = renderSecondaryNav();
+      const buttonsNode = renderButtonsNode();
+
+      return (
+        <StyledToolbar hasInvertedStyle={hasInvertedStyle}>
+          {logoNode}
+
+          {!isNarrow && (
+            <>
+              <StyledPrimaryNavContainer
+                primaryNavPosition={primaryNavPosition}
+              >
+                {primaryNavPosition === "left" ? (
+                  <>
+                    {primaryNav}
+                    {search}
+                  </>
+                ) : (
+                  <>
+                    {search}
+                    {primaryNav}
+                  </>
+                )}
+              </StyledPrimaryNavContainer>
+
+              {secondaryNav}
+              {buttonsNode}
+            </>
+          )}
+
+          {isNarrow && (
+            <StyledNarrowButton
               sdsType="secondary"
-              icon={buttonProps.icon}
-              aria-label={String(buttonProps.children)}
+              sdsStyle="icon"
+              icon={drawerOpen ? "XMark" : "LinesHorizontal3"}
+              onClick={() => setDrawerOpen((prev) => !prev)}
               customTheme={
                 hasInvertedStyle
                   ? mode === "light"
@@ -164,86 +258,36 @@ export default function NavigationHeader<T extends string = string>({
                   : "auto"
               }
             />
-          );
-        }
+          )}
+        </StyledToolbar>
+      );
+    };
 
-        return (
-          <StyledHeaderButton
-            key={key}
-            sx={fullWidth}
-            sdsStyle="rounded"
-            {...(buttonProps as SdsMinimalButtonProps | SdsButtonProps)}
-            hasInvertedStyle={hasInvertedStyle}
-          />
-        );
-      })}
-    </StyledButtonSection>
-  );
+    const headerContent = renderHeaderContent();
 
-  const headerContent = (
-    <StyledToolbar hasInvertedStyle={hasInvertedStyle}>
-      {logoNode}
-
-      {!isNarrow && (
-        <>
-          <StyledPrimaryNavContainer primaryNavPosition={primaryNavPosition}>
-            {primaryNavPosition === "left" ? (
-              <>
-                {primaryNav}
-                {search}
-              </>
-            ) : (
-              <>
-                {search}
-                {primaryNav}
-              </>
-            )}
-          </StyledPrimaryNavContainer>
-
-          {secondaryNav}
-          {buttonsNode}
-        </>
-      )}
-
-      {isNarrow && (
-        <StyledNarrowButton
-          sdsType="secondary"
-          sdsStyle="icon"
-          icon={drawerOpen ? "XMark" : "LinesHorizontal3"}
-          onClick={() => setDrawerOpen((prev) => !prev)}
-          customTheme={
-            hasInvertedStyle ? (mode === "light" ? "dark" : "light") : "auto"
-          }
-        />
-      )}
-    </StyledToolbar>
-  );
-
-  return (
-    <>
-      <StyledHeader
-        elevation={0}
-        position="static"
-        hasInvertedStyle={hasInvertedStyle}
-      >
-        {headerContent}
-      </StyledHeader>
-
-      {isNarrow && drawerOpen && (
-        <StyledDrawer
-          anchor="right"
-          open={drawerOpen}
-          onClose={() => setDrawerOpen(false)}
+    return (
+      <div ref={ref}>
+        <StyledHeader
+          elevation={0}
+          position="static"
           hasInvertedStyle={hasInvertedStyle}
         >
           {headerContent}
+        </StyledHeader>
 
-          {search}
-          {primaryNav}
-          {secondaryNav}
-          {buttonsNode}
-        </StyledDrawer>
-      )}
-    </>
-  );
-}
+        {isNarrow && drawerOpen && (
+          <StyledDrawer
+            anchor="right"
+            open={drawerOpen}
+            onClose={() => setDrawerOpen(false)}
+            hasInvertedStyle={hasInvertedStyle}
+          >
+            {headerContent}
+          </StyledDrawer>
+        )}
+      </div>
+    );
+  }
+);
+
+export default NavigationHeader;
