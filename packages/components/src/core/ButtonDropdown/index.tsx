97ba7f58efee4754aafef6bb4b597b8f88a18e02
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2025-02-13 09:55:55 -0800

    feat(NavigationHeader): implement NavigationHeader component (#885)
    * feat(navigationbar): add new component
    
    - WIP, support API for modifying home image, links, and responsiveness
    
    re #728
    
    * feat(header): implements desktop header
    
    * feat(header): mobile header implementation
    
    * refactor(navigationheader): renames to NavigationHeader
    
    * fix(navigationheader): fix lint error
    
    * refactor(navigationheader): rename story title
    
    * refactor(navigationheader): rename to isNarrow
    
    * fix(navigationheader): address styling issues
    
    * refactor(navigationheader): use object props instead of ReactNode
    
    * fix(navigationheader): remove unused import
    
    * fix(navigationheader): fix build error
    
    * refactor(navigationheader): inverted style prop
    
    * fix(navigationheader): remove unused imports
    
    * test(navigationheader): update snapshots
    
    * test(navigationheader): fix axe test
    
    * fix(navigationheader): address design feedback
    
    * fix(navigationheader): fixed colors and added new customTheme for Buttons and InputSearch
    
    * fix(navigationheader): fixed Chevron position in secondary nav items
    
    * feat(designtokens): added new inverse colors to design tokens
    
    * fix(navigationheader): fixed prop forwarding issues
    
    * fix(navigationheader): fixed Button and ButtonDropdown types
    
    Added tests and coverred all available controls in the storybook for the NavigationHeader component
    
    * fix(navigationheader): added tests and fixed bugs
    
    * fix(inputdropdown): fix styles
    
    * fix(inputdropdown): fixed button styles
    
    * fix(navigationheader): fixed design review bugs and finalized the component
    
    * feat(navigationheader): added auto switch between mobile and desktop if there is not enough space
    
    * fix(navigationheader): fixed style errors
    
    * fix(navigationheader): fixed primary nav items hover style
    
    ---------
    
    Co-authored-by: Andrew Holloway <booc0mtaco@users.noreply.github.com>
    Co-authored-by: Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>

--- packages/components/src/core/ButtonDropdown/index.tsx
+++ packages/components/src/core/ButtonDropdown/index.tsx
@@ -1,6 +1,5 @@
-import { ButtonProps as RawButtonProps } from "@mui/material";
 import React, { ForwardedRef } from "react";
-import Button from "src/core/Button";
+import Button, { ButtonProps } from "src/core/Button";
 import Icon, { IconNameToSizes, IconProps } from "src/core/Icon";
 import ButtonIcon from "../ButtonIcon";
 import {
@@ -8,71 +7,69 @@ import {
   showWarningIfFirstOccurence,
 } from "src/common/warnings";
 
-type SdsProps = {
-  icon?: keyof IconNameToSizes | React.ReactElement<CustomSVGProps>;
-  sdsStyle?: "rounded" | "square" | "icon";
-  sdsType?: "primary" | "secondary";
-  sdsIconProps?: Partial<IconProps<keyof IconNameToSizes>>;
-  sdsSize?: "small" | "medium" | "large";
-};
+type SDSButtonDropdownSizes = "small" | "medium" | "large";
 
-export type ButtonDropdownProps<C extends React.ElementType = "button"> =
-  RawButtonProps<C, { component?: C }> & SdsProps;
+type SdsProps =
+  | {
+      sdsStyle: "icon";
+      sdsType?: "primary" | "secondary" | "tertiary";
+      icon: keyof IconNameToSizes | React.ReactElement<CustomSVGProps>;
+      sdsIconProps?: Partial<IconProps<keyof IconNameToSizes>>;
+      sdsSize?: SDSButtonDropdownSizes;
+    }
+  | {
+      sdsStyle: "rounded" | "square";
+      sdsType?: "primary" | "secondary" | "destructive";
+      icon?: keyof IconNameToSizes | React.ReactElement<CustomSVGProps>;
+      sdsIconProps?: Partial<IconProps<keyof IconNameToSizes>>;
+      sdsSize?: SDSButtonDropdownSizes;
+    };
 
-type IconSizeType = "xs" | "s" | "l" | "xl";
-
-const BUTTON_ICON_SIZE_TO_SDS_ICON_SIZE = {
-  large: "xl",
-  medium: "l",
-  small: "s",
-};
+export type ButtonDropdownProps = ButtonProps & SdsProps;
 
 /**
  * @see https://mui.com/material-ui/react-button/
  */
 const ButtonDropdown = React.forwardRef(
-  <C extends React.ElementType>(
-    props: ButtonDropdownProps<C>,
+  (
+    props: ButtonDropdownProps,
     ref: ForwardedRef<HTMLButtonElement>
   ): JSX.Element | null => {
-    const { icon, sdsStyle, sdsType, sdsSize = "medium", sdsIconProps } = props;
-    const iconSize = BUTTON_ICON_SIZE_TO_SDS_ICON_SIZE[sdsSize];
-
-    const iconItem = () => {
-      if (icon) {
-        if (typeof icon !== "string") {
-          return icon;
-        } else {
-          return (
-            <Icon
-              {...sdsIconProps}
-              sdsIcon={icon}
-              sdsSize={iconSize as IconSizeType}
-            />
-          );
-        }
-      }
-    };
+    const { sdsStyle } = props;
 
     if (sdsStyle === "icon") {
-      if (icon !== undefined) {
-        return <ButtonIcon icon={icon} {...props} ref={ref} />;
-      } else {
+      if (!props.icon) {
         showWarningIfFirstOccurence(SDSWarningTypes.ButtonIconMissingIconProp);
         return null;
       }
-    } else {
+      return <ButtonIcon {...props} ref={ref} />;
+    }
+
+    if (sdsStyle === "square" || sdsStyle === "rounded") {
+      const iconItem = () => {
+        if (props?.icon) {
+          return typeof props?.icon === "string" ? (
+            <Icon {...props?.sdsIconProps} sdsIcon={props?.icon} sdsSize="xl" />
+          ) : (
+            props?.icon
+          );
+        }
+        return null;
+      };
+
       return (
         <Button
           {...props}
           endIcon={<Icon sdsIcon="ChevronDown" sdsSize="xs" />}
           sdsStyle={sdsStyle}
           ref={ref}
-          sdsType={sdsType}
           startIcon={iconItem()}
         />
       );
     }
+
+    showWarningIfFirstOccurence(SDSWarningTypes.ButtonDropdownMinimal);
+    return null;
   }
 );
 
