0c7c48e318ce08d6539308afbda91ed97912c89b
Masoud Amjadi <masoudmanson@gmail.com>
2025-05-01 19:55:51 -0400

    feat: Bar Chart + ScatterPlot initial implementation

--- packages/data-viz/src/index.ts
+++ packages/data-viz/src/index.ts
@@ -4,5 +4,8 @@ export * from "./core/StackedBarChart";
 export { default as StackedBarChart } from "./core/StackedBarChart";
 export * from "./core/HistogramChart";
 export { default as HistogramChart } from "./core/HistogramChart";
-
+export * from "./core/BarChart";
+export { default as BarChart } from "./core/BarChart";
+export * from "./core/ScatterPlot";
+export { default as ScatterPlot } from "./core/ScatterPlot";
 
