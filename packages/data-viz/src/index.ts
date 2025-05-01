ddc41a4098faff87ffae07bdda0f42f9c9b212c2
Masoud Amjadi <masoudmanson@gmail.com>
2025-05-01 19:55:51 -0400

    feat: Bar Chart + ScatterPlot initial implementation

--- packages/data-viz/src/index.ts
+++ packages/data-viz/src/index.ts
@@ -2,3 +2,7 @@ export * from "./core/HeatmapChart";
 export { default as HeatmapChart } from "./core/HeatmapChart";
 export * from "./core/HistogramChart";
 export { default as HistogramChart } from "./core/HistogramChart";
+export * from "./core/BarChart";
+export { default as BarChart } from "./core/BarChart";
+export * from "./core/ScatterPlot";
+export { default as ScatterPlot } from "./core/ScatterPlot";
