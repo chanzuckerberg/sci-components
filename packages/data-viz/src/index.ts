5e7e4e74b4633663a0394f5b68cc76ef102fac6f
Masoud Amjadi <masoudmanson@gmail.com>
2025-05-01 19:22:22 -0400

    feat(HistogramChart): Initial implementation

--- packages/data-viz/src/index.ts
+++ packages/data-viz/src/index.ts
@@ -1,4 +1,8 @@
 export * from "./core/HeatmapChart";
-export { default as HeatmapChart } from "./core/HeatmapChart";
+export { default as HeatmapChart } from "./core/HeatmapChart"
 export * from "./core/StackedBarChart";
 export { default as StackedBarChart } from "./core/StackedBarChart";
+export * from "./core/HistogramChart";
+export { default as HistogramChart } from "./core/HistogramChart";
+
+
