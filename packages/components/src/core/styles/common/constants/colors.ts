df1aa31ababac574ba5458e4123eb15be25351eb
Masoud Amjadi <masoudmanson@gmail.com>
2025-08-02 15:16:07 -0400

    fix(colors): revert gray600 to #6c6c6c until we figure out the right color
    The new color (#7a7a7a) causes contrast issues and a11y errors

--- packages/components/src/core/styles/common/constants/colors.ts
+++ packages/components/src/core/styles/common/constants/colors.ts
@@ -18,7 +18,7 @@ export const SDSLightThemeColors: Colors = {
     "400": "#a5a5a5",
     "50": "#ffffff",
     "500": "#969696",
-    "600": "#7a7a7a",
+    "600": "#6C6C6C",
     "700": "#3b3b3b",
     "75": "#f8f8f8",
     "800": "#1b1b1b",
