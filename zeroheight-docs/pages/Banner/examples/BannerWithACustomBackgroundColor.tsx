9522c466167de07c89ca02db66000bff87e36fd5
Masoud Amjadi <masoudmanson@gmail.com>
2026-07-29 12:39:07 -0400

    docs(banner): refactor banner docs

--- zeroheight-docs/pages/Banner/examples/BannerWithACustomBackgroundColor.tsx
+++ zeroheight-docs/pages/Banner/examples/BannerWithACustomBackgroundColor.tsx
@@ -1,10 +1,11 @@
 import { styled } from "@mui/material/styles";
 import { Banner } from "@czi-sds/components";
 
+const StyledBanner = styled(Banner)`
+  background-color: Crimson;
+`;
+
 function App() {
-  const StyledBanner = styled(Banner)`
-    background-color: Crimson;
-  `;
   return (
     <div className="app">
       <StyledBanner sdsType="primary">
